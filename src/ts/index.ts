66cf3723ce3ddcf37233c8c1ca3abf7f29b79c26
Danila <mok-danil28@mail.ru>
2024-07-29 18:45:41 +0300

    Cleared code

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,26 +3,28 @@ import "../index.html";
 
 //Slider
 
-const sliderNext = document.getElementById("next-arrow") as HTMLButtonElement;
-const sliderPrev = document.getElementById("prev-arrow") as HTMLButtonElement;
-const sliderInner = document.querySelector(".testimonials__slider") as HTMLElement;
-const card = document.querySelector(".testimonials-card") as HTMLElement;
-const paginationButtons = document.querySelectorAll(".testimonials-pagination__button");
-let currentIndex = 0;
+const sliderNext = document.getElementById("next-arrow") as HTMLButtonElement,
+    sliderPrev = document.getElementById("prev-arrow") as HTMLButtonElement,
+    sliderInner = document.querySelector(".testimonials__slider") as HTMLElement,
+    card = document.querySelector(".testimonials-card") as HTMLElement,
+    paginationButtons = document.querySelectorAll(".testimonials-pagination__button") as NodeListOf<HTMLButtonElement>;
+
+let currentIndex: number = 0;
 
 window.addEventListener('resize', () => {
     currentIndex = 0;
     moveSlider(0);
     sliderPrev.disabled = false;
     sliderNext.disabled = false;
-})
+});
+
 sliderNext.addEventListener('click', () => {
     currentIndex += 1;
     sliderPrev.disabled = false;
     if (currentIndex == 2) {
         sliderNext.disabled = true;
     }
-    let sliderTranslate = (card.offsetWidth + 50) * -currentIndex;
+    let sliderTranslate: number = (card.offsetWidth + 50) * -currentIndex;
     moveSlider(sliderTranslate);
 })
 
@@ -33,22 +35,22 @@ sliderPrev.addEventListener('click', () => {
     if (currentIndex == -2) {
         sliderPrev.disabled = true;
     }
-    let sliderTranslate = -(card.offsetWidth + 50) * currentIndex;
+    let sliderTranslate: number = -(card.offsetWidth + 50) * currentIndex;
     moveSlider(sliderTranslate);
 })
 
 function moveSlider(sliderTranslate: number) {
     sliderInner.style.transform = `translateX(${sliderTranslate}px)`;
-    paginationButtons.forEach((item,i) => {
+    paginationButtons.forEach((item: HTMLButtonElement, i: number) => {
         item.classList.remove("is-active");
-        if (i == currentIndex + 2) {
+        if (i === currentIndex + 2) {
             item.classList.add("is-active");
         }
     });
 
 }
 
-paginationButtons.forEach((item,i) => {
+paginationButtons.forEach((item: HTMLButtonElement, i: number) => {
     item.addEventListener('click', () => {
         currentIndex = i-2;
         if (currentIndex > 0) {
