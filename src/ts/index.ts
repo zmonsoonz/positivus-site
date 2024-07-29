89813888aa6acc715ee22c584372261447d6f9a6
Danila <mok-danil28@mail.ru>
2024-07-29 16:01:41 +0300

    Made burger-menu

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,15 @@
 import "../styles/styles.scss";
-import "../index.html"
+import "../index.html";
+
+//Slider
+
+const sliderNext = document.querySelector(".next-arrow") as Element;
+const sliderPrev = document.querySelector(".prev-arrow") as Element;
+const sliderInner = document.querySelector(".testimonials__slider") as HTMLElement;
+const card = document.querySelector(".testimonials-card") as HTMLElement;
+const sliderTranslate = card.offsetWidth*2;
+console.log(sliderTranslate);
+
+sliderNext.addEventListener('click', () => {
+    sliderInner.style.left = `${sliderTranslate}px`;
+})
