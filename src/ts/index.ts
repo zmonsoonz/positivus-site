19c93fbd71b6ed6ed57a7823e0862970a970ce82
Danila <mok-danil28@mail.ru>
2024-07-29 17:13:48 +0300

    Made slider

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,13 +3,62 @@ import "../index.html";
 
 //Slider
 
-const sliderNext = document.querySelector(".next-arrow") as Element;
-const sliderPrev = document.querySelector(".prev-arrow") as Element;
+const sliderNext = document.getElementById("next-arrow") as HTMLButtonElement;
+const sliderPrev = document.getElementById("prev-arrow") as HTMLButtonElement;
 const sliderInner = document.querySelector(".testimonials__slider") as HTMLElement;
 const card = document.querySelector(".testimonials-card") as HTMLElement;
-const sliderTranslate = card.offsetWidth*2;
-console.log(sliderTranslate);
+const paginationButtons = document.querySelectorAll(".testimonials-pagination__button");
+let currentIndex = 0;
 
-sliderNext.addEventListener('click', () => {
-    sliderInner.style.left = `${sliderTranslate}px`;
+window.addEventListener('resize', () => {
+    currentIndex = 0;
+    moveSlider(0);
+    sliderPrev.disabled = false;
+    sliderNext.disabled = false;
 })
+sliderNext.addEventListener('click', () => {
+    currentIndex += 1;
+    sliderPrev.disabled = false;
+    if (currentIndex == 2) {
+        sliderNext.disabled = true;
+    }
+    let sliderTranslate = (card.offsetWidth + 50) * -currentIndex;
+    moveSlider(sliderTranslate);
+})
+
+
+sliderPrev.addEventListener('click', () => {
+    currentIndex -= 1;
+    sliderNext.disabled = false;
+    if (currentIndex == -2) {
+        sliderPrev.disabled = true;
+    }
+    let sliderTranslate = -(card.offsetWidth + 50) * currentIndex;
+    moveSlider(sliderTranslate);
+})
+
+function moveSlider(sliderTranslate: number) {
+    sliderInner.style.transform = `translateX(${sliderTranslate}px)`;
+    paginationButtons.forEach((item,i) => {
+        item.classList.remove("is-active");
+        if (i == currentIndex + 2) {
+            item.classList.add("is-active");
+        }
+    });
+
+}
+
+paginationButtons.forEach((item,i) => {
+    item.addEventListener('click', () => {
+        currentIndex = i-2;
+        if (currentIndex > 0) {
+            let sliderTranslate = (card.offsetWidth + 50) * -currentIndex;
+            moveSlider(sliderTranslate);
+        }
+        else {
+            let sliderTranslate = -(card.offsetWidth + 50) * currentIndex;
+            moveSlider(sliderTranslate);
+        }
+    })
+})
+
